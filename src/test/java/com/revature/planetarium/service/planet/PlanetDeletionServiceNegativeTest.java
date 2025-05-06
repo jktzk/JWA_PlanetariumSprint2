3e41a1c2a65a05a267cee3798157487a3daee578
Preston T <prestontrevino89@gmail.com>
2025-05-06 10:30:06 -0500

    finished planet deletion negative test- 1: failed test

--- src/test/java/com/revature/planetarium/service/planet/PlanetDeletionServiceNegativeTest.java
+++ src/test/java/com/revature/planetarium/service/planet/PlanetDeletionServiceNegativeTest.java
@@ -10,41 +10,42 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RunWith(Parameterized.class)
 public class PlanetDeletionServiceNegativeTest {
     private PlanetDao planetDao;
     private PlanetService planetService;
-
+    private Planet planetEntity;
     private Planet stubbedPlanet;
 
-    @Parameterized.Parameter(4)
-    public Exception exception;
+    private static final List<Planet> mockList = new ArrayList<>();
+
+
     @Parameterized.Parameter(3)
     public String message;
     @Parameterized.Parameter(2)
     public boolean wasDeleted;
     @Parameterized.Parameter(1)
-    public boolean booleanCheck1;
+    public int booleanCheck1;
     @Parameterized.Parameter
-    public boolean isEmpty;
+    public List<Planet> listMockList;
 
 
 
     @Parameterized.Parameters
     public static Object[][] inputs(){
         return new Object[][]{
-                {true,true,true,"Planet delete failed",new SQLException()},
-                {true,true,true,"Invalid planet name",null},
-                {false,false,true,"Invalid planet name",null},
-                {false,false,false,"Could not delete the planet",null},
+//                if the planet does not exist
+                {Collections.emptyList(),4,true,"Invalid planet name"},
+//                if the user tries to delete a planet they don't own
+                {mockList,1,true,"Invalid planet name"},
+//                if all data validations pass but the DAO does not return a true
+                {mockList,1,false,"Could not delete the planet"},
 
 
         };
@@ -53,31 +54,39 @@ public class PlanetDeletionServiceNegativeTest {
     @Before
     public void setup() throws IOException, InterruptedException {
         TestUtilities.resetDatabase();
+
         planetDao = Mockito.mock(PlanetDaoImp.class);
+        planetEntity = Mockito.mock(Planet.class);
         planetService = new PlanetServiceImp(planetDao);
         stubbedPlanet = new Planet(4, "Nerf",4);
+        mockList.add(stubbedPlanet);
 
     }
 
     @Test
     public void deletePlanetServicePositiveTest() throws SQLException {
-        List<Planet> posPlanetList = Arrays.asList(stubbedPlanet);
 
-//        mocking SQLException
-        if (exception != null){
-            Mockito.when(planetDao.readPlanetsByOwner(stubbedPlanet.getOwnerId())).thenThrow(exception);
-        }
 //        mocking the readPlanetByOwner method in ln:74
-        Mockito.when(planetDao.readPlanetsByOwner(stubbedPlanet.getOwnerId()).isEmpty()).thenReturn(isEmpty);
+        Mockito.when(planetDao.readPlanetsByOwner(stubbedPlanet.getOwnerId())).thenReturn(listMockList);
+//
 //        mocking the p.getPlanetName().equals(planetName) && p.getOwnerId() == userId in ln:80
-        Mockito.when(stubbedPlanet.getPlanetName().equals(stubbedPlanet.getPlanetName())).thenReturn(booleanCheck1);
-        Mockito.when(stubbedPlanet.getOwnerId()).thenReturn(stubbedPlanet.getOwnerId());
+        Mockito.when(planetEntity.getPlanetName()).thenReturn(stubbedPlanet.getPlanetName());
+        Mockito.when(planetEntity.getOwnerId()).thenReturn(booleanCheck1);
+//
 //        mocking the deletePlanet() method ln:82
         Mockito.when(planetDao.deletePlanet(stubbedPlanet.getPlanetName())).thenReturn(wasDeleted);
 
-        boolean result = planetService.deletePlanet(stubbedPlanet.getOwnerId(),stubbedPlanet.getPlanetName());
-        PlanetFail exception = Assert.assertThrows(PlanetFail.class,() -> planetService.deletePlanet(stubbedPlanet.getOwnerId(),stubbedPlanet.getPlanetName()));
-        Assert.assertFalse(result);
-        Assert.assertEquals(message,exception.getMessage());
+//        Testing the service level now that mocks are set up
+        boolean result = true;
+        PlanetFail exception = new PlanetFail("no exception thrown");
+        try {
+            result = planetService.deletePlanet(stubbedPlanet.getOwnerId(), stubbedPlanet.getPlanetName());
+        } catch (Exception e) {
+            exception = Assert.assertThrows(PlanetFail.class, () -> planetService.deletePlanet(stubbedPlanet.getOwnerId(), stubbedPlanet.getPlanetName()));
+
+        }
+//        -----------------------------
+        Assert.assertTrue(result);
+        Assert.assertEquals(message, exception.getMessage());
     }
 }
