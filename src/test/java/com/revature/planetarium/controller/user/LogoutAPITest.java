477c63dcc17ad45a3982449710b79ca0b93948af
A. Montesino <120063688+amontesino@users.noreply.github.com>
2025-05-07 16:55:09 -0400

    implemented logout test

---
+++ src/test/java/com/revature/planetarium/controller/user/LogoutAPITest.java
@@ -0,0 +1,17 @@
+package com.revature.planetarium.controller.user;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+
+public class LogoutAPITest {
+
+    @Test
+    public void logoutTest() {
+        RestAssured
+                .when().post("logout")
+                .then().header("Content-Type", IsEqual.equalTo(ContentType.JSON.toString()))
+                .statusCode(401);
+    }
+}
