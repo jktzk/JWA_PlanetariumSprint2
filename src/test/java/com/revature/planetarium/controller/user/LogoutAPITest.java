c19b7fae92d5cc4f83c2b68c8a8873cb2e0be4b1
A. Montesino <120063688+amontesino@users.noreply.github.com>
2025-05-09 15:08:28 -0400

    added authentication pre-logout for ensuring test is written correctly

--- src/test/java/com/revature/planetarium/controller/user/LogoutAPITest.java
+++ src/test/java/com/revature/planetarium/controller/user/LogoutAPITest.java
@@ -1,15 +1,25 @@
 package com.revature.planetarium.controller.user;
 
+import com.revature.planetarium.controller.APIFixture;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.core.IsEqual;
+import org.junit.Before;
 import org.junit.Test;
 
-public class LogoutAPITest {
+public class LogoutAPITest extends APIFixture {
+
+    private String sessionId;
+
+    @Before
+    public void setup() {
+        sessionId = super.authentication("Batman", "Iamthenight1939");
+    }
 
     @Test
     public void logoutTest() {
-        RestAssured
+        RestAssured.given()
+                .cookie("JSESSIONID", sessionId)
                 .when().post("logout")
                 .then().statusCode(401);
     }
