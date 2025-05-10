832cdb7a1130e5d3f88efec1c933ef8c65edc4e9
Preston T <prestontrevino89@gmail.com>
2025-05-10 15:31:35 -0500

    finished planet and moon

--- src/test/java/com/revature/planetarium/controller/moon/DeleteMoonAPITest.java
+++ src/test/java/com/revature/planetarium/controller/moon/DeleteMoonAPITest.java
@@ -1,7 +1,8 @@
-package com.revature.planetarium.controller.planet;
+package com.revature.planetarium.controller.moon;
+
 
 import com.revature.planetarium.controller.APIFixture;
-import com.revature.planetarium.controller.PlanetController;
+import com.revature.planetarium.controller.MoonController;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -14,63 +15,62 @@ import org.junit.runners.Parameterized;
 import java.io.IOException;
 
 @RunWith(Parameterized.class)
-public class DeletePlanetAPITest extends APIFixture{
+public class DeleteMoonAPITest extends APIFixture {
 
-
-    private PlanetController planetController;
     private APIFixture apiFixture;
+    private MoonController moonController;
     private String code;
 
+
     @Parameterized.Parameter
-    public int ownerID;
+    public int planetID;
     @Parameterized.Parameter(1)
     public String username;
     @Parameterized.Parameter(2)
     public String password;
     @Parameterized.Parameter(3)
-    public String planetName;
+    public String identifier;
     @Parameterized.Parameter(4)
     public int statusCode;
     @Parameterized.Parameter(5)
     public String expectedResponse;
 
-
     @Parameterized.Parameters
     public static Object[][] inputs(){
         return new Object[][]{
-            {1,"Batman","Iamthenight1939","Earth",204,""},
-                {2,"Batman","Iamthenight1939","Earth",400,"Invalid planet name"},
-                {1,"Batman","Iamthenight1939","FakeEarth",400,"Invalid planet name"},
+                {1,"Batman","Iamthenight1939","Luna",204,""},
+                {2,"Batman","Iamthenight1939","Luna",404,"Invalid moon name"},
+                {1,"Batman","Iamthenight1939","FakeLuna",404,"Invalid planet identifier"},
         };
     }
-
 
     @Before
     public void setup() throws IOException, InterruptedException {
         super.resetDatabase();
+        Thread.sleep(200);
         code = super.authentication(username,password);
     }
 
     @Test
-    public void planetDeleteAPIPositiveTest(){
+    public void deleteMoonAPITest(){
 
-        // get response from http delete request
+
+        //so in documentations was told to test url:/planetarium/planet/{planetId}/moon/{identifier}
+        //actual url for deletion of moon url: /planetarium/moon/{identifier}
+
         Response response = RestAssured.given()
-                .cookie("JSESSIONID", code)
+                .cookie("JSESSIONID",code)
                 .when()
-                .delete("/planetarium/user/" + ownerID +"/planet/" + planetName)
+                .delete("/planetarium/planet/" + planetID +"/moon/" + identifier)
                 .then()
                 .statusCode(statusCode)
                 .extract()
                 .response();
 
         // take that response and try to make it a json
-             JsonPath jSonPath = response.jsonPath();
+        JsonPath jsonPath = response.jsonPath();
 
         // assert that the expected json response is given by pulling the variable from the json
-        Assert.assertEquals(expectedResponse,jSonPath.getString("message"));
-
-
+        Assert.assertEquals(expectedResponse,jsonPath.getString("message"));
     }
-
 }
